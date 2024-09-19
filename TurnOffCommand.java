447018dfa3611003c3369bbad35e139f255916e5
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2024-09-19 23:43:35 +0800

    Create TurnOffCommand.java

---
+++ TurnOffCommand.java
@@ -0,0 +1,12 @@
+public class TurnOffCommand implements Command {
+    private SmartDevice device;
+
+    public TurnOffCommand(SmartDevice device) {
+        this.device = device;
+    }
+
+    @Override
+    public void execute() {
+        device.turnOff();
+    }
+}
