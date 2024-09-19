d137e9a324b63dd11d8e32babeebe05942b16c73
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2024-09-19 23:43:19 +0800

    Create TurnOnCommand.java

---
+++ TurnOnCommand.java
@@ -0,0 +1,12 @@
+public class TurnOnCommand implements Command {
+    private SmartDevice device;
+
+    public TurnOnCommand(SmartDevice device) {
+        this.device = device;
+    }
+
+    @Override
+    public void execute() {
+        device.turnOn();
+    }
+}
